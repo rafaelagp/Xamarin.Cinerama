2f279aed9546410d2e2fe0a72e8da401a51db17a
Rafael Pereira <rafgpereira@gmail.com>
2017-08-17 06:43:18 -0300

    More style improvements.

---
+++ source/iOS/Renderers/NoSelectionViewCellRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Cinerama.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(NoSelectionViewCellRenderer))]
+namespace Cinerama.iOS.Renderers
+{
+	public class NoSelectionViewCellRenderer : ViewCellRenderer
+	{
+		public override UIKit.UITableViewCell GetCell(Cell item, UIKit.UITableViewCell reusableCell, UIKit.UITableView tv)
+		{
+			var cell = base.GetCell(item, reusableCell, tv);
+			cell.SelectionStyle = UIKit.UITableViewCellSelectionStyle.None;
+
+			return cell;
+		}
+	}
+}
