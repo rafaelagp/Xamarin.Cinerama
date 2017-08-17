ca7a31a6acc0562e31a79b2bf8dbafa9ede0afd3
Rafael Pereira <rafgpereira@gmail.com>
2017-08-16 23:22:24 -0300

    Added an activity indicator to the upcoming movies screen.

--- source/Cinerama/ViewModels/BaseViewModel.cs
+++ source/Cinerama/ViewModels/BaseViewModel.cs
@@ -6,6 +6,7 @@ namespace Cinerama.ViewModels
 	public class BaseViewModel
 	{
 		public bool IsBusy { get; set; }
+		public bool IsNotBusy { get { return !IsBusy; } }
 		public string Title { get; set; } = "Cinerama";
 	}
 }
