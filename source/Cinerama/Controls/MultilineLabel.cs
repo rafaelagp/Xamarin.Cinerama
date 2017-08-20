838b906aa5f4770b889b36afba1e7a68ff30dd6f
Rafael Pereira <rafgpereira@gmail.com>
2017-08-19 23:34:12 -0300

    Created a multiline label control.

---
+++ source/Cinerama/Controls/MultilineLabel.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Cinerama.Controls
+{
+	public class MultilineLabel : Label
+	{
+		public int MaxLines
+		{
+			get { return (int)GetValue(MaxLinesProperty); }
+			set { SetValue(MaxLinesProperty, value); }
+		}
+
+		public static readonly BindableProperty MaxLinesProperty = BindableProperty.Create(nameof(MaxLines),
+		                                                                                   typeof(int),
+		                                                                                   typeof(MultilineLabel),
+		                                                                                   0);
+	}
+}
---
+++ source/Droid/Renderers/MultilineLabelRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Cinerama.Controls;
+using Cinerama.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MultilineLabel), typeof(MultilineLabelRenderer))]
+namespace Cinerama.Droid.Renderers
+{
+	public class MultilineLabelRenderer : LabelRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null && e.NewElement != null)
+			{
+				Control.SetMaxLines(((MultilineLabel)e.NewElement).MaxLines);
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName.Equals(MultilineLabel.MaxLinesProperty))
+			{
+				Control.SetMaxLines(((MultilineLabel)Element).MaxLines);
+			}
+		}
+	}
+}
