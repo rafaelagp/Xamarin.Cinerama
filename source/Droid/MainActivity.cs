9bf3a3cff370ad893a899e4fd9a79b0c6fce07a2
Rafael Pereira <rafgpereira@gmail.com>
2017-08-20 21:07:08 -0300

    Implemented Android splash screen.

--- source/Droid/MainActivity.cs
+++ source/Droid/MainActivity.cs
@@ -1,24 +1,30 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 using Cinerama.Droid.Utils;
 using Cinerama.Interfaces;
 using Microsoft.Practices.Unity;
 using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace Cinerama.Droid
 {
-	[Activity(Label = "Cinerama", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	[Activity(Label = "Cinerama", Icon = "@drawable/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
 		{
+			base.Window.RequestFeature(WindowFeatures.ActionBar);
+			base.SetTheme(Resource.Style.MyTheme);
+
 			TabLayoutResource = Resource.Layout.Tabbar;
 			ToolbarResource = Resource.Layout.Toolbar;
 
 			base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init(this, bundle);
+			Forms.Init(this, bundle);
 
 			LoadApplication(new App(new PrismInitializer()));
 		}
